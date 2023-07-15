204038084b9767162ef2c1f4d74eb572bced779a
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-16 03:55:38 +0800

    feat: use cache control
    just so i don't forget when i build it later

--- scumjournal/src/routes/index.tsx
+++ scumjournal/src/routes/index.tsx
@@ -1,25 +1,36 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import {component$} from "@builder.io/qwik";
+import type {DocumentHead, RequestHandler} from "@builder.io/qwik-city";
+
+export const onGet: RequestHandler = async ({cacheControl}) => {
+    // Control caching for this request for best performance and to reduce hosting costs:
+    // https://qwik.builder.io/docs/caching/
+    cacheControl({
+        // Always serve a cached response by default, up to a week stale
+        staleWhileRevalidate: 60 * 60 * 24 * 7,
+        // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+        maxAge: 5,
+    });
+};
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
-  );
+    return (
+        <>
+            <h1>Hi 👋</h1>
+            <p>
+                Can't wait to see what you build with qwik!
+                <br/>
+                Happy coding.
+            </p>
+        </>
+    );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+    title: "Welcome to Qwik",
+    meta: [
+        {
+            name: "description",
+            content: "Qwik site description",
+        },
+    ],
 };
