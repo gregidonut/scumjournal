f3fc813f5a6d80343058335cc54b46d6fe37b0ee
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-17 02:46:38 +0800

    feat: add strcuture for new article
    this makes use of a new Blog Articles component
    i imagine this would be where i should fetch info from databases(to follow)

---
+++ scumjournal/src/components/BlogArticles/BlogArticle.tsx
@@ -0,0 +1,27 @@
+import {component$, useStyles$} from "@builder.io/qwik";
+import {Link} from "@builder.io/qwik-city";
+import styles from "./styles.css?inline";
+import favicon from "../../../public/favicon.svg?inline"
+import portrait from "./portrait-pixelate-4.png?inline"
+
+export default component$(() => {
+    useStyles$(styles)
+    return (
+        <>
+            <article>
+                <section>
+                    <img width="300" height="300" src={portrait} alt="Display Picture"/>
+                </section>
+               <section>
+                   <h3>starting a blog</h3>
+                   <p>anchoring my self into creating more</p>
+               </section>
+            </article>
+            <article>
+                <section>
+                    <img width="300" height="300" src={favicon} alt="Display Picture"/>
+                </section>
+            </article>
+        </>
+    );
+});
--- scumjournal/src/components/Main/Main.tsx
+++ scumjournal/src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import {component$, useStyles$} from "@builder.io/qwik";
 import styles from "./styles.css?inline";
-import displayPicture from "./portrait-pixelate-4.png?inline"
+import displayPicture from "../BlogArticles/portrait-pixelate-4.png?inline"
+import BlogArticle from "~/components/BlogArticles/BlogArticle";
 
 
 export default component$(() => {
@@ -8,17 +9,7 @@ export default component$(() => {
     return (
         <>
             <main>
-                <article>
-                    <section>
-                        <img width="300" height="300" src={displayPicture} alt="Display Picture"/>
-                    </section>
-                    <section>
-                        <h3>starting a blog</h3>
-                        <p>
-                            trying to anchor myself into creating more
-                        </p>
-                    </section>
-                </article>
+                <BlogArticle/>
             </main>
         </>
     );
