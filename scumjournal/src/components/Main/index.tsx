128f02b92996f51c82ef8bf421bfd38c03a1aac9
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-16 21:53:07 +0800

    feat: add basic debuggable styles to Footer and Main
    just to make it easier

--- scumjournal/src/components/Main/index.tsx
+++ scumjournal/src/components/Main/index.tsx
@@ -1,7 +1,10 @@
-import {component$} from "@builder.io/qwik";
+import {component$, useStyles$} from "@builder.io/qwik";
 import type {DocumentHead} from "@builder.io/qwik-city";
+import styles from "./styles.css?inline";
+
 
 export default component$(() => {
+    useStyles$(styles)
     return (
         <>
             <main>
