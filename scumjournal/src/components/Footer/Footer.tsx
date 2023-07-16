136df81a932a090685e0b1f463fbd9d46a1cf45a
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-17 06:15:08 +0800

    feat(styles): adjust footer height and add link to github
    thats good enough for now

--- scumjournal/src/components/Footer/Footer.tsx
+++ scumjournal/src/components/Footer/Footer.tsx
@@ -1,10 +1,16 @@
-import {component$} from "@builder.io/qwik";
+import {component$, useStyles$} from "@builder.io/qwik";
+import {Link} from "@builder.io/qwik-city";
+import styles from "./styles.css?inline";
 
 export default component$(() => {
+    useStyles$(styles)
     return (
         <>
             <footer>
-                <p>Greg Osilaja</p>
+                <p> <Link href="/about">Greg Osilaja</Link>
+                    <span>|</span>
+                    <Link href="https://github.com/gregidonut">Github</Link>
+                </p>
             </footer>
         </>
     );
