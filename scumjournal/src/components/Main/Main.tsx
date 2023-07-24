62d2b6872c48faed4ae11c7c710124c62a0e04a0
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-25 03:14:50 +0800

    chore: complete the refactor of the MicroBlogArticle component
    should be capitalized to follow common convention as well as the usage in the
    Main component

--- scumjournal/src/components/Main/Main.tsx
+++ scumjournal/src/components/Main/Main.tsx
@@ -1,7 +1,7 @@
 import {component$, useStylesScoped$} from "@builder.io/qwik";
 import styles from "./styles.css?inline";
-// import displayPicture from "../microBlogArticles/portrait-pixelate-4.png?inline"
-import BlogArticle from "~/components/microBlogArticles/microBlogArticles";
+// import displayPicture from "../MicroBlogArticles/portrait-pixelate-4.png?inline"
+import MicroBlogArticle from "~/components/MicroBlogArticles/MicroBlogArticles";
 
 
 export default component$(() => {
@@ -9,7 +9,7 @@ export default component$(() => {
     return (
         <>
             <main id="blog-page">
-                <BlogArticle/>
+                <MicroBlogArticle/>
             </main>
         </>
     );
