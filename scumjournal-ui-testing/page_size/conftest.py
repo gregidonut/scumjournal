3a2865d999fab9d33695a8e2597209834c167a0e
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-09 15:46:39 +0800

    test: handle that ConnectionError from webdriver-manager
    To make to retry the test since installing the webdriver sometimes
    results in an error

--- scumjournal-ui-testing/page_size/conftest.py
+++ scumjournal-ui-testing/page_size/conftest.py
@@ -3,9 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from requests.exceptions import ConnectionError
 
 IMPLICIT_WAIT_TIME = 10
 URL = "http://localhost:5173/"
+MAX_RETRIES = 3
 
 
 @pytest.fixture
@@ -17,11 +19,21 @@ def default_options():
 
 
 def instantiate_driver(options: Options) -> webdriver.Chrome:
+    service: str = ""
+    for i in range(MAX_RETRIES):
+        try:
+            service = ChromeDriverManager().install()
+            break
+        except ConnectionError:
+            if i + 1 == MAX_RETRIES:
+                print(f"retried 3 times")
+                raise
+            print(f"retried {i + 1} times")
+
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
+        service=Service(service),  # TODO: figure out a better naming convention here
         options=options,
     )
-
     driver.implicitly_wait(IMPLICIT_WAIT_TIME)
     driver.get(URL)
 
