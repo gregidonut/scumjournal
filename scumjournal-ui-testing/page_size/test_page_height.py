d57e3cb37e81dedc164e7faa14de34063a1b1beb
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-09 16:38:57 +0800

    test: refactor logic into a helper function
    Making sure to keep the assert statements in the test functions, but extracted the logic
    of scrolling and getting the page height to make the whole thing more atomic

--- scumjournal-ui-testing/page_size/test_page_height.py
+++ scumjournal-ui-testing/page_size/test_page_height.py
@@ -1,36 +1,40 @@
-def test_body_height_on_desktop(desktop_driver):
-    driver = desktop_driver
+from selenium import webdriver
+
+
+def body_and_window_height(scroll_to_bottom: bool, driver: webdriver.Chrome):
+    if scroll_to_bottom:
+        # scroll to bottom of page targeting main tag
+        # since body tag cant be scrolled because of it's size
+        driver.execute_script(
+            """
+            document.querySelector('main').scrollTo(
+                0, 
+                document.querySelector('main').scrollHeight,
+            );
+            """
+        )
+
     body_height = driver.execute_script("return document.body.offsetHeight")
     window_height = driver.execute_script("return window.innerHeight")
-    assert body_height == window_height
 
-    # scroll to bottom of page targeting main tag
-    # since body tag cant be scrolled because of it's size
-    driver.execute_script(
-        """
-        document.querySelector('main').scrollTo(
-            0, 
-            document.querySelector('main').scrollHeight,
-        );
-        """
-    )
-    assert body_height == window_height
+    return body_height, window_height
+
+
+def test_body_height_on_desktop(desktop_driver):
+    driver = desktop_driver
+
+    for s in [False, True]:
+        body_height, window_height = body_and_window_height(
+            scroll_to_bottom=s, driver=driver
+        )
+        assert body_height == window_height
 
 
 def test_body_height_on_mobile(mobile_driver):
     driver = mobile_driver
-    body_height = driver.execute_script("return document.body.offsetHeight")
-    window_height = driver.execute_script("return window.innerHeight")
-    assert body_height == window_height
 
-    # scroll to bottom of page targeting main tag
-    # since body tag cant be scrolled because of it's size
-    driver.execute_script(
-        """
-        document.querySelector('main').scrollTo(
-            0, 
-            document.querySelector('main').scrollHeight,
-        );
-        """
-    )
-    assert body_height == window_height
+    for s in [False, True]:
+        body_height, window_height = body_and_window_height(
+            scroll_to_bottom=s, driver=driver
+        )
+        assert body_height == window_height
