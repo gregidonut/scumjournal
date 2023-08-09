ce70c2ac88ba43cc6d68d3ba50f3b9dd649179c3
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-09 14:05:13 +0800

    test: add test cases for mobile emulation
    Using a separate fixture since i can see how there might be a complete
    different set of configurations for desktop

--- scumjournal-ui-testing/page_size/conftest.py
+++ scumjournal-ui-testing/page_size/conftest.py
@@ -23,3 +23,28 @@ def desktop_driver():
     yield driver
 
     driver.quit()
+
+
+@pytest.fixture(params=[
+    "iPhone SE",
+    "iPad Mini",
+])
+def mobile_driver(request):
+    device_name = request.param
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_experimental_option(
+        "mobileEmulation", {"deviceName": device_name}
+    )
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options,
+    )
+    driver.implicitly_wait(IMPLICIT_WAIT_TIME)
+    driver.get(URL)
+
+    yield driver
+
+    driver.quit()
--- scumjournal-ui-testing/page_size/test_page_height.py
+++ scumjournal-ui-testing/page_size/test_page_height.py
@@ -3,3 +3,10 @@ def test_body_height_on_desktop(desktop_driver):
     body_height = driver.execute_script('return document.body.offsetHeight')
     window_height = driver.execute_script('return window.innerHeight')
     assert body_height == window_height
+
+
+def test_body_height_on_mobile(mobile_driver):
+    driver = mobile_driver
+    body_height = driver.execute_script('return document.body.offsetHeight')
+    window_height = driver.execute_script('return window.innerHeight')
+    assert body_height == window_height
