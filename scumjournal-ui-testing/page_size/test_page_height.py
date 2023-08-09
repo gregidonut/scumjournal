938a7a097fc8372cc0763d5c874978d2ae263746
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-09 16:22:20 +0800

    test: add second assert statement in testcases
    This one asserts after the page has been scrolled to the bottom

--- scumjournal-ui-testing/page_size/test_page_height.py
+++ scumjournal-ui-testing/page_size/test_page_height.py
@@ -4,9 +4,33 @@ def test_body_height_on_desktop(desktop_driver):
     window_height = driver.execute_script("return window.innerHeight")
     assert body_height == window_height
 
+    # scroll to bottom of page targeting main tag
+    # since body tag cant be scrolled because of it's size
+    driver.execute_script(
+        """
+        document.querySelector('main').scrollTo(
+            0, 
+            document.querySelector('main').scrollHeight,
+        );
+        """
+    )
+    assert body_height == window_height
+
 
 def test_body_height_on_mobile(mobile_driver):
     driver = mobile_driver
     body_height = driver.execute_script("return document.body.offsetHeight")
     window_height = driver.execute_script("return window.innerHeight")
+    assert body_height == window_height
+
+    # scroll to bottom of page targeting main tag
+    # since body tag cant be scrolled because of it's size
+    driver.execute_script(
+        """
+        document.querySelector('main').scrollTo(
+            0, 
+            document.querySelector('main').scrollHeight,
+        );
+        """
+    )
     assert body_height == window_height
