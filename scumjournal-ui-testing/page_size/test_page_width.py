0629df357da15fc4ee883f24529b75a0d71ce163
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-09 18:07:49 +0800

    test: add check for 100% width with body against window and main
    This is in addition to body against header to cover more bases

--- scumjournal-ui-testing/page_size/test_page_width.py
+++ scumjournal-ui-testing/page_size/test_page_width.py
@@ -10,11 +10,43 @@ def body_and_header_width(driver: webdriver.Chrome):
     return body_width, header_width
 
 
-def test_body_header_width_on_desktop(desktop_driver):
+def body_and_main_width(driver: webdriver.Chrome):
+    body_width = driver.execute_script("return document.body.offsetWidth")
+    main_width = driver.execute_script(
+        "return document.querySelector('main').offsetWidth"
+    )
+
+    return body_width, main_width
+
+
+def body_and_window_width(driver: webdriver.Chrome):
+    body_width = driver.execute_script("return document.body.offsetWidth")
+    window_width = driver.execute_script(
+        "return window.innerWidth"
+    )
+
+    return body_width, window_width
+
+
+def test_body_and_header_width_on_desktop(desktop_driver):
     driver = desktop_driver
 
     body_width, header_width = body_and_header_width(driver)
     assert body_width == header_width
+
+
+def test_body_and_main_width_on_desktop(desktop_driver):
+    driver = desktop_driver
+
+    body_width, main_width = body_and_main_width(driver)
+    assert body_width == main_width
+
+
+def test_body_and_window_width_on_desktop(desktop_driver):
+    driver = desktop_driver
+
+    body_width, window_width = body_and_window_width(driver)
+    assert body_width == window_width
 
 
 def test_body_height_on_mobile(mobile_driver):
@@ -22,3 +54,17 @@ def test_body_height_on_mobile(mobile_driver):
 
     body_width, header_width = body_and_header_width(driver)
     assert body_width == header_width
+
+
+def test_body_and_main_on_mobile(mobile_driver):
+    driver = mobile_driver
+
+    body_width, main_width = body_and_main_width(driver)
+    assert body_width == main_width
+
+
+def test_body_and_window_width_on_mobile(mobile_driver):
+    driver = mobile_driver
+
+    body_width, window_width = body_and_window_width(driver)
+    assert body_width == window_width
