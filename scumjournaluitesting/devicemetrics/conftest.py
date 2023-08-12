2e6325c644ab736695bc197c187a37963d105f90
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-12 17:48:33 +0800

    test: refactor new fixtures into a conftest.py file
    Just to make the tests more atomic

---
+++ scumjournaluitesting/devicemetrics/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from .device_metrics import DeviceName, new_device
+
+DEVICE_NAME_INSTANCE_TO_BE_TESTED = DeviceName.iPhone_SE
+
+
+@pytest.fixture
+def new_device_instance():
+    return (
+        new_device(
+            device=DEVICE_NAME_INSTANCE_TO_BE_TESTED,
+            width=320,
+            height=568,
+            pixel_ratio=2.0,
+        )
+    )
+
+
+@pytest.fixture(params=[
+
+    (DeviceName.Samsung_Galaxy_A51slash71, "Samsung Galaxy A51/71"),
+    (DeviceName.Samsung_Galaxy_S8plus, "Samsung Galaxy S8+"),
+])
+def special_symbol_names(request):
+    return request.param
--- scumjournaluitesting/devicemetrics/test_device_metrics.py
+++ scumjournaluitesting/devicemetrics/test_device_metrics.py
@@ -1,4 +1,3 @@
-import pytest
 from .device_metrics import DeviceName, new_device
 
 DEVICE_NAME_INSTANCE_TO_BE_TESTED = DeviceName.iPhone_SE
@@ -11,18 +10,6 @@ DEVICE_METRICS_DICT_FROM_INSTANCE = {
 }
 
 
-@pytest.fixture
-def new_device_instance():
-    return (
-        new_device(
-            device=DEVICE_NAME_INSTANCE_TO_BE_TESTED,
-            width=320,
-            height=568,
-            pixel_ratio=2.0,
-        )
-    )
-
-
 def test_new_device(new_device_instance):
     assert new_device_instance.name == DEVICE_NAME_INSTANCE_TO_BE_TESTED.name
 
@@ -31,22 +18,13 @@ def test_device_metrics(new_device_instance):
     assert new_device_instance.get_device_metrics() == DEVICE_METRICS_DICT_FROM_INSTANCE
 
 
-@pytest.fixture(params=[
-
-    (DeviceName.Samsung_Galaxy_A51slash71, "Samsung Galaxy A51/71"),
-    (DeviceName.Samsung_Galaxy_S8plus, "Samsung Galaxy S8+"),
-])
-def special_symboled_names(request):
-    return request.param
-
-
-def test_special_symbols_in_enum_name(special_symboled_names):
-    enum, expected = special_symboled_names
+def test_special_symbols_in_enum_name(special_symbol_names):
+    enum, expected = special_symbol_names
     assert enum.name == expected
 
 
-def test_special_symbols_in_enum_from_new_device_constructor(special_symboled_names):
-    enum, expected = special_symboled_names
+def test_special_symbols_in_enum_from_new_device_constructor(special_symbol_names):
+    enum, expected = special_symbol_names
 
     device = new_device(
         device=enum,
